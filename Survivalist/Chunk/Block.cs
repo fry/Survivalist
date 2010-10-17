86667277e1c2ad3519bc40b25bf1cd1d93588701
Eike Siewertsen <e.siew@londeroth.org>
2010-10-17 14:17:45 +0200

    Comment update routine for water

--- Survivalist/Chunk/Block.cs
+++ Survivalist/Chunk/Block.cs
@@ -112,6 +112,7 @@ namespace Survivalist {
 		}
 
 		public override void OnUpdate(World world, int x, int y, int z) {
+			// Get all kinds of required data
 			var type = world.GetBlockType(x, y, z);
 			var groundtype = world.GetBlockType(x, y - 1, z);
 			var metaData = world.GetBlockData(x, y, z);
@@ -123,9 +124,11 @@ namespace Survivalist {
 			// 1) It can't flow on top of water tiles
 			// 2) It can't spread if it is falling
 			if (type == (int)BlockType.Water) {
+				// Not falling tiles adjust their size depending on their neighbors' height
 				if (!isFalling) {
 					var maxNHeight = FindMaxNHeight(world, x, y, z);
-					if (maxNHeight >= height) { // this block is higher than our highest neighbor
+					if (maxNHeight >= height) { // this block is higher than our highest neighbor and needs to shrink
+						// If this already is the smallest block, disappear, otherwise shrink by 1
 						if (height == 7)
 							world.SetBlockType(x, y, z, (int)BlockType.Air);
 						else {
@@ -135,13 +138,16 @@ namespace Survivalist {
 						}
 						return;
 					} else if (height != maxNHeight + 1) { // this block needs to grow
+						// Never grow higher than the max height
 						world.SetBlockData(x, y, z, Math.Min(7, maxNHeight + 1));
 						ActivateNeighbors(world, x, y, z);
 						ActivateBlock(world, x, y, z);
 					}
 				} else {
+					// Falling water shrinks if there is no water block above it
 					var aboveType = world.GetBlockType(x, y + 1, z);
 					if (aboveType != (int)BlockType.Water && aboveType != (int)BlockType.StillWater) {
+						// Falling water shrinks faster than normal tiles
 						if (height >= 6)
 							world.SetBlockType(x, y, z, (int)BlockType.Air);
 						else {
@@ -153,26 +159,28 @@ namespace Survivalist {
 					}
 				}
 
+				// Flowing water doesn't spread on top of water
 				if (onWater)
 					return;
 			}
 
+			// Set height of all following tiles and abort if this tile is already the smallest
 			height++;
-			// PROBLEM: water doesn't flow into sideway directions if placed in air
-			// Only flow either up or horizontally in one tick, and never flow if we're already of the smallest kind
 			if (height >= 8)
 				return;
+			// Source blocks also spread on top of water
 			bool flowDown = DoFlow(world, x, y - 1, z, 8);
 			if (!flowDown || (onWater && type == (int)BlockType.StillWater)) {
+				// Try flowing towards the nearest drop in 4 tiles radius	
 				if (!FlowRadius(world, x, y, z, 1, height))
 					if (!FlowRadius(world, x, y, z, 2, height))
 						if (!FlowRadius(world, x, y, z, 3, height))
 							if (!FlowRadius(world, x, y, z, 4, height)) {
-								// No drop down, flow in all directions
-								bool didFlow = DoFlow(world, x - 1, y, z, height);
-								didFlow = DoFlow(world, x + 1, y, z, height) || didFlow;
-								didFlow = DoFlow(world, x, y, z - 1, height) || didFlow;
-								didFlow = DoFlow(world, x, y, z + 1, height) || didFlow;
+								// No nearby drop down, flow in all directions
+								DoFlow(world, x - 1, y, z, height);
+								DoFlow(world, x + 1, y, z, height);
+								DoFlow(world, x, y, z - 1, height);
+								DoFlow(world, x, y, z + 1, height);
 							}
 				return;
 			}
@@ -181,17 +189,19 @@ namespace Survivalist {
 			ActivateBlock(world, x, y, z);
 		}
 
+		// The first 7 bits of the meta data specify the height (0 is a full block, 7 is a nearly empty block)
 		protected int GetHeight(int metaData) {
 			return metaData & 7;
 		}
 
+		// The 8th bit of the meta data specifies whether this is a falling tile
 		protected bool IsFalling(int metaData) {
 			return (metaData & 8) != 0;
 		}
 
+		// Return the height of the highest tile adjacent to this tile
 		protected int FindMaxNHeight(World world, int x, int y, int z) {
 			int maxNHeight = 7;
-
 			CheckTile(world, x - 1, y, z, ref maxNHeight);
 			CheckTile(world, x + 1, y, z, ref maxNHeight);
 			CheckTile(world, x, y, z - 1, ref maxNHeight);
@@ -200,6 +210,7 @@ namespace Survivalist {
 			return maxNHeight;
 		}
 
+		// If the passed tile is higher than the passed height, adjust that height
 		protected void CheckTile(World world, int x, int y, int z, ref int height) {
 			var type = world.GetBlockType(x, y, z);
 			if (type == (int)BlockType.Water)
@@ -208,10 +219,13 @@ namespace Survivalist {
 				height = 0;
 		}
 
+		// Return whether this type is interpreted as a "drop" and should be flowed towards
 		protected bool IsDrop(int type) {
 			return type == (int)BlockType.Air || type == (int)BlockType.Water || type == (int)BlockType.StillWater;
 		}
 
+		// Look for a drop on the sides of a rectangle with the passed radius and flow towards it
+		// Also return whether we did find a drop
 		protected bool FlowRadius(World world, int x, int y, int z, int radius, int metaData) {
 			bool didFlow = false;
 			int checkY = y - 1;
@@ -238,6 +252,7 @@ namespace Survivalist {
 			return didFlow;
 		}
 
+		// Try filling the passed tile with water and return whether we're successful
 		protected bool DoFlow(World world, int x, int y, int z, int metaData) {
 			var type = world.GetBlockType(x, y, z);
 			if (type == (int)BlockType.Water || type == (int)BlockType.StillWater)
